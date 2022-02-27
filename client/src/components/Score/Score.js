60eeee783c30a19e4f472612e9d5200088d1ef52
leticianardi <leticia.tnardi@gmail.com>
2022-02-27 12:36:15 -0800

    setting up for scores

--- client/src/components/Score/Score.js
+++ client/src/components/Score/Score.js
@@ -1,25 +1,41 @@
-// import { useContext, useEffect, useState } from "react";
-// import { GlobalSettingsContext } from "../hooks/useGlobalSettings";
-// import { GuessDistributionKeys, StatisticsContext } from "../hooks/useStatistics";
+// // import { useContext, useEffect, useState } from "react";
+// // import { GlobalSettingsContext } from "../hooks/useGlobalSettings";
+// // import { GuessDistributionKeys, StatisticsContext } from "../hooks/useStatistics";
 
-export default function Score() {
+// import { Redirect, useParams } from "react-router-dom";
+// import { useQuery, useMutation } from "@apollo/client";
+// import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
- const handleClick = (e) => {
-  console.log(e)
- }
-  return (
-    <div>
-     <h1>Scores</h1>
+// export default function Score() {
+//   // grab user + friends informartion from database
 
+//   // grab User score from localStorage
 
-    <div>
-     <button onClick={handleClick} data-id='0'>today</button>
-     <button onClick={handleClick} data-id='7'>this week</button>
-     <button onClick={handleClick} data-id='30'>this month</button>
-     <button onClick={handleClick} data-id='365'>all time</button>
-    </div>
+//   // display scores and sort them
 
-    </div>
-  )
-}
+//   const handleClick = (e) => {
+//     console.log(e);
+//   };
+//   return (
+//     <div>
+//       <h1>Scores</h1>
 
+//       <div>
+//         <button onClick={handleClick} data-id="0">
+//           today
+//         </button>
+//         <button onClick={handleClick} data-id="7">
+//           this week
+//         </button>
+//         <button onClick={handleClick} data-id="30">
+//           this month
+//         </button>
+//         <button onClick={handleClick} data-id="365">
+//           all time
+//         </button>
+//       </div>
+
+//       <Profiles Leaderboard={Leaderboard}></Profiles>
+//     </div>
+//   );
+// }
