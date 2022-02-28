0e43a8f7fe9bd98d50fc63bd5d6fca9713537223
leticianardi <leticia.tnardi@gmail.com>
2022-02-27 17:33:06 -0800

    merging styles to main

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PostList from "../components/PostList/postList";
+import PostList from "../components/PostList/PostList";
 
 import { useQuery } from "@apollo/client";
 import { QUERY_POSTS } from "../utils/queries";
