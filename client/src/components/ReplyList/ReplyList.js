b33aae66861de5732bab70cae6e652cfbb1dfc3b
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-28 19:59:43 -1000

    updates to previous push

--- client/src/components/ReplyList/ReplyList.js
+++ client/src/components/ReplyList/ReplyList.js
@@ -3,15 +3,23 @@ import { Link } from "react-router-dom";
 
 const ReplyList = ({ replies}) => {
   return (
+    <div>
+    <div>
       <span>Reply board</span>
-  );
+    </div>
+    <div>
       {replies &&
         replies.map((replies) => (
-          <p className="/" key={replies._id}>
-            {replies.repliesBody} {"// "}
+          <p key={replies._id}>
+            {replies.replyBody} {"// "}
               {replies.username} on {replies.createdAt}
           </p>
         ))}
+    </div>
+  </div>
+
+  );
+  
 };
 
 export default ReplyList
