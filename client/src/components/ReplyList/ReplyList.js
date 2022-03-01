a229b3cb88f19af7cb42ad9b1ba8bc139f7ebce8
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-28 19:50:47 -1000

    reply board push

--- client/src/components/ReplyList/ReplyList.js
+++ client/src/components/ReplyList/ReplyList.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const ReplyList = ({ replies}) => {
   return (
-    <div>ReplyList</div>
-  )
-}
+    <div className="card mb-3">
+    <div className="card-header">
+      <span className="text-light">Reply board</span>
+    </div>
+    <div className="card-body">
+      {replies &&
+        replies.map((replies) => (
+          <p className="pill mb-3" key={replies._id}>
+            {replies.repliesBody} {"// "}
+              {replies.username} on {replies.createdAt}
+          </p>
+        ))}
+    </div>
+  </div>
+
+  );
+  
+};
 
 export default ReplyList
