d9c64c7a0212145a96ea32edc5c57bc4e04cad0c
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-28 19:54:27 -1000

    updates to class

--- client/src/components/ReplyList/ReplyList.js
+++ client/src/components/ReplyList/ReplyList.js
@@ -3,23 +3,15 @@ import { Link } from "react-router-dom";
 
 const ReplyList = ({ replies}) => {
   return (
-    <div className="card mb-3">
-    <div className="card-header">
-      <span className="text-light">Reply board</span>
-    </div>
-    <div className="card-body">
+      <span>Reply board</span>
+  );
       {replies &&
         replies.map((replies) => (
-          <p className="pill mb-3" key={replies._id}>
+          <p className="/" key={replies._id}>
             {replies.repliesBody} {"// "}
               {replies.username} on {replies.createdAt}
           </p>
         ))}
-    </div>
-  </div>
-
-  );
-  
 };
 
 export default ReplyList
